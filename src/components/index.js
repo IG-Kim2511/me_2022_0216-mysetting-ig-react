a64ce247865dcd557d11d9ada83419273b12aa7d
IG <iikim2511@gmail.com>
2022-02-16 23:15:08 -0500

    ...

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+
+// 폴더내의 파일들 index.js안으로 모아서 import,export 
+
+import SubHeading from './SubHeading/SubHeading';
+import Newsletter from './Footer/Newsletter';
+import FooterOverlay from './Footer/FooterOverlay';
+import MenuItem from './MenuItem/MenuItem';
+import Navbar from './Navbar/Navbar';
+
+export {
+  SubHeading,
+  Newsletter,
+  FooterOverlay,
+  MenuItem,
+  Navbar,
+};
