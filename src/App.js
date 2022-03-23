28d7dff2414d20a8d214df9c75020bcea6eea6a7
IG <iikim2511@gmail.com>
2022-03-23 16:49:02 -0400

    reset

--- src/App.js
+++ src/App.js
@@ -6,36 +6,37 @@ import { Awards, Intro, Navbar } from './container-sections';
 import './App.css';
 
 
+
+
+//  {/* js 1208. ~~~~~ */}
 function App() {
-
   const [items, setItems] = useState([]);
-  
-  useEffect(() => {
-    const fetchItems = async()=>{
-      const result = await axios('https://thronesapi.com/api/v2/Characters')
-  
-      console.log(result.data);     
-      setItems(result.data)
-    }
-  
-    fetchItems()
-  }, [])
-  
-  return (
 
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios("https://thronesapi.com/api/v2/Characters");
+
+      console.log(result.data);
+      setItems(result.data);
+    };
+
+    fetchItems();
+  }, []);
+
+  return (
     <div>
-      {/*🍀  test */}      
-      <h1 className="" style={{ background: 'yellow'}}>test</h1>
+      {/*🍀  test */}
+      <h1 className="" style={{ background: "yellow" }}>
+        test
+      </h1>
 
       {/* containers , components*/}
 
-      <Navbar/>
-      <Intro/>
-      <Awards/> 
+      <Navbar />
+      <Intro />
+      <Awards />
 
       <div className="navbar"></div>
-      
-      
     </div>
   );
 }
